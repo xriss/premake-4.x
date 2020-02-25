2e00794cd9913f6d01d3e22635ae8a761531aa6d
kriss <kriss@xixs.com>
2020-02-25 18:52:04 +0000

    .

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 BASEPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $BASEPATH/tests && luajit $BASEPATH/src/host/premake.lua /file=./premake4.lua /scripts=.. $1 $2 $3 test
+cd $BASEPATH/tests && $BASEPATH/puremake.sh /scripts=.. $* test
 
