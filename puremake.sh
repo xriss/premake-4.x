57251ba454f9105b50c9e00bfdc357287ce36090
kriss <kriss@xixs.com>
2020-02-25 19:38:40 +0000

    .

--- puremake.sh
+++ puremake.sh
@@ -1,12 +1,16 @@
 BASEPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+# you can set PUREMAKE_LUA to the lua you wish to use, or we will try and find one
+
 if [ -z "$PUREMAKE_LUA" ]; then 
  if command -v luajit; then
   PUREMAKE_LUA=luajit
  elif command -v lua; then 
   PUREMAKE_LUA=lua
+ elif command -v gamecake; then 
+  PUREMAKE_LUA=gamecake
  else
-  printf "This program requires lua or luajit to be installed\n" 1>&2
+  printf "puremake requires luajit or lua or gamecake to be installed\n" 1>&2
   exit 1
  fi
 fi
